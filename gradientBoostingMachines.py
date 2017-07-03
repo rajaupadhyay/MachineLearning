f5267dd7d7a80d7c4fb13335faac6b837d64e4fe
rajaupadhyay <upadhyayaraja@gmail.com>
2017-07-03 21:59:43 +0300

    Random forests

---
+++ gradientBoostingMachines.py
@@ -0,0 +1,29 @@
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target,random_state=0)
+
+grbt = GradientBoostingClassifier(random_state=0)
+
+grbt.fit(X_train, y_train)
+
+print("Training set accuracy: {:.2f}".format(grbt.score(X_train, y_train)))
+print("Test set accuracy: {:.2f}".format(grbt.score(X_test, y_test)))
+# 100% accuracy on training set: Overfitting - We can lower the learning rate to reduce overfitting or use stronger
+# pre-pruning (limit max depth)
+
+grbt1 = GradientBoostingClassifier(random_state=0, max_depth=1)
+grbt1.fit(X_train, y_train)
+
+print("Training set accuracy: {:.2f}".format(grbt1.score(X_train, y_train)))
+print("Test set accuracy: {:.2f}".format(grbt1.score(X_test, y_test)))
+
+grbt2 = GradientBoostingClassifier(random_state=0, learning_rate=0.01)
+grbt2.fit(X_train, y_train)
+
+print("Training set accuracy: {:.2f}".format(grbt2.score(X_train, y_train)))
+print("Test set accuracy: {:.2f}".format(grbt2.score(X_test, y_test)))
+
